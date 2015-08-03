0023400f76912ec2a325ef28a50683fda069620a
PaulingZhou <paulingzhou@gmail.com>
2015-08-03 11:06:26 +0800

    commit solution of reverse integer
    accept by leetcode

--- 7 Reverse Integer/Solution.java
+++ 7 Reverse Integer/Solution.java
@@ -1,27 +1,13 @@
 public class Solution {
     public int reverse(int x) {
-    	while(x % 10 == 0){
-    		x /= 10;
+    	int newResult = 0,result = 0;
+    	while(x != 0){
+    		newResult = result * 10 + x % 10;
+    		if((newResult - x%10) != result * 10)
+    			return 0;
+    		result = newResult;
+    		x/=10;
     	}
-    	boolean isPositive = true;
-        if(x < 0){
-        	x = -x;
-        	isPositive = false;
-        }
-        String s = x+"";
-        int sLength = s.length();
-        char c[] = s.toCharArray();
-        for(int i = 0;i < sLength/2; i++){
-        	char temp = c[i];
-        	c[i] = c[sLength - i - 1];
-        	c[sLength - i - 1] = temp;
-        }
-        s = new String(c);
-        x = Integer.parseInt(s);
-        if(x < 0)
-        	return 0;
-        else if(!isPositive)
-        	x = -x;
-        return x;
+    	return newResult;
     }
 }
--- 7 Reverse Integer/Test.java
+++ 7 Reverse Integer/Test.java
@@ -1,6 +1,6 @@
 class Test{
 	public static void main(String[] args) {
-		int x = -200001;
+		int x = -123;
 		System.out.println(new Solution().reverse(x));	
 	}
 }
