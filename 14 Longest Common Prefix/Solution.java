3dff5f3400385a772d7e14c88baf622985ec0194
PaulingZhou <paulingzhou@gmail.com>
2015-08-05 08:24:53 +0800

    commit  solution of Longest Common Prefix
    accepted by Leetcode

--- 14 Longest Common Prefix/Solution.java
+++ 14 Longest Common Prefix/Solution.java
@@ -1,24 +1,15 @@
 public class Solution {
     public String longestCommonPrefix(String[] strs) {
     	if(strs == null || strs.length == 0)return "";
-    	int length = strs.length;
-    	int maxLength = 0,temp = 0;
-    	for(int i = 0;i < length;i++){
-    		System.out.println("strs[" + i + "]-->" + strs[i]);
-    		for(int j = 0;j < strs[i].length();j++){
-    			if(strs[i].charAt(j) == '-'){
-    				if(maxLength < j){
-    					temp = i;
-    					maxLength = j;
-    				}
-    				break;
-    			}
-    			if(maxLength == j)
-   				temp = -1;
+    	String commonPrefix = strs[0];
+    	for(int i = 0 ; i < strs.length ; i++){
+    		int j = 0;
+    		while(j < commonPrefix.length() && j < strs[i].length()){
+    			if(commonPrefix.charAt(j) != strs[i].charAt(j))break;
+    			j++;
     		}
+    		commonPrefix = commonPrefix.substring(0,j);
     	}
-    	System.out.println(temp);
-    	if(temp == -1)return "";
-        return strs[temp];
+    	return commonPrefix;
     }
 }
--- 14 Longest Common Prefix/Test.java
+++ 14 Longest Common Prefix/Test.java
@@ -1,6 +1,6 @@
 class Test{
 	public static void main(String[] args) {
-		String[] s = {"a"};
+		String[] s = {"a","b"};
 		System.out.println(new Solution().longestCommonPrefix(s));
 	}
 }
