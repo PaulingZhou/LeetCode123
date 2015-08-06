d68ca7d1f57457db5d716125b10e92594f53109a
PaulingZhou <paulingzhou@gmail.com>
2015-08-06 23:39:27 +0800

    commit solution of remove element
    think more about  for loop

---
+++ 27 Remove Element/Solution.java
@@ -0,0 +1,12 @@
+package com.zhou.remove_element;
+
+public class Solution {
+    public int removeElement(int[] nums, int val) {
+		int count = 0;
+		for(int i = 0 ; i < nums.length ; i++){
+			if(nums[i] != val)nums[count++] = nums[i];
+		}
+		for(int i = 0 ; i < count ; i++)System.out.println(nums[i]);
+    	return count;  
+    }
+}
---
+++ 27 Remove Element/Test.java
@@ -0,0 +1,9 @@
+package com.zhou.remove_element;
+
+public class Test {
+	public static void main(String[] args) {
+		int[] nums = {1,4,5};
+		Solution solution = new Solution();
+		solution.removeElement(nums, 4);
+	}
+}
