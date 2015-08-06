5876c56533d69357e810d7ed61b0b219d7170fc0
PaulingZhou <paulingzhou@gmail.com>
2015-08-06 14:46:00 +0800

    commit solution of valid parantheses
    accepted by LeetCode

--- 20 Valid Parentheses/Solution.java
+++ 20 Valid Parentheses/Solution.java
@@ -1,5 +1,41 @@
+import java.util.Stack;
+
 public class Solution {
     public boolean isValid(String s) {
-        
+        if (s == null || s.length() == 0)
+            return true;
+        else {
+            char[] c = s.toCharArray();
+            Stack<Character> stack = new Stack<Character>();
+            for (int i = 0; i < c.length; i++) {
+                if (c[i] == '{' || c[i] == '[' || c[i] == '(') {
+                    stack.push(c[i]);
+                } else{
+                    switch (c[i]) {
+                    case '}':
+                        if (!stack.empty() && stack.peek() == '{')
+                            stack.pop();
+                        else
+                            return false;
+                        break;
+                    case ')':
+                        if (!stack.empty() && stack.peek() == '(')
+                            stack.pop();
+                        else
+                            return false;
+                        break;
+                    case ']':
+                        if (!stack.empty() && stack.peek() == '[')
+                            stack.pop();
+                        else
+                            return false;
+                        break;
+                    default:
+                        break;
+                    }
+                }
+            }
+            return stack.empty();
+        }
     }
 }
